df77ad26844949f868be1af291068686729f53c6
Surendar N <119476295+SurendarNehru@users.noreply.github.com>
2025-06-22 21:10:17 +0530

    Add files via upload

---
+++ week1/exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class FinancialForecast
+{
+    public static double CalculateFutureValue(double initialamount, double annualrate, int totalyears)
+    {
+        if (totalyears == 0)
+            return initialamount;
+        return (1 + annualrate) * CalculateFutureValue(initialamount, annualrate, totalyears - 1);
+    }
+    public static void Main(string[] args)
+    {
+        double initialinvestment = 2500;
+        double annualgrowthrate = 0.5;
+        int forecastyear = 7;
+
+        double projectedValue = CalculateFutureValue(initialinvestment, annualgrowthrate, forecastyear);
+        Console.WriteLine("Predicted Future Value after " + forecastyear + " years: ₹" + Math.Round(projectedValue, 2));
+    }
+}
