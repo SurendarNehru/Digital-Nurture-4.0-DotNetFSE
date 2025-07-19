c6fdf14de66edd0225b9f8ac4cb767021240d2c3
Surendar N <119476295+SurendarNehru@users.noreply.github.com>
2025-07-19 11:43:38 +0530

    Add files via upload

---
+++ week5/lab 6/Sender.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+class Sender
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+        using var sender = new ProducerBuilder<Null, string>(config).Build();
+
+        Console.WriteLine("Type a message :");
+
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input)) continue;
+            if (input.ToLower() == "exit") break;
+
+            var result = await sender.ProduceAsync("chat-topic", new Message<Null, string> { Value = input });
+            Console.WriteLine($"Sent: {result.Value}");
+        }
+    }
+}
