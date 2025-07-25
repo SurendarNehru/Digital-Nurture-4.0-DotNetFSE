561bce7eabe0b4b0d36be6ff9f18d483e1f52747
Surendar N <119476295+SurendarNehru@users.noreply.github.com>
2025-07-25 15:57:04 +0530

    Add files via upload

---
+++ week6/lab4/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders blog posts heading', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/blog posts/i);
+  expect(headingElement).toBeInTheDocument();
+});
---
+++ week6/lab4/Posts.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ posts: data });
+      })
+      .catch(error => {
+        console.error("Error loading posts:", error);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    alert('An error occurred in Posts component');
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
